0e41013fdbfcd44558b0cd6459e4f08d1df9537c
arny <arny@example.com>
2021-10-18 22:08:11 +0300

    added gin and handlers

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"log"
 
-	"github.com/ainurqa95/go-todo-list-app"
+	"github.com/ainurqa95/todo-list-app"
+	"github.com/ainurqa95/todo-list-app/pkg/handler"
 )
 
 func main() {
 
 	srv := new(todo.Server)
+	handlers := new(handler.Handler)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
